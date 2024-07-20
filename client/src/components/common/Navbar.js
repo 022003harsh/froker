de7f48c216e698e7eea0665795a4f15f47317d8a
Harsh <harshku2222@gmail.com>
2024-07-20 17:23:04 +0530

    feat: Navbar for mobile view

--- client/src/components/common/Navbar.js
+++ client/src/components/common/Navbar.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/images/black-logo-frokerr-copy-10_pwpomg.jpg.png';
+import { HiX } from 'react-icons/hi';
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const links = [
     { name: 'Home', path: '/' },
     { name: 'Blogs', path: '/blog' },
@@ -12,12 +16,43 @@ const Navbar = () => {
   return (
     <div className='flex h-16 items-center justify-between w-[95%] max-w-maxContent mx-auto'>
       <div className='flex items-center justify-between w-full h-full'>
-        <Link to="/">
-          <img src={logo} alt="Logo" width={115} height={24} loading="lazy" className='pt-2'/>
+      <Link to="/" className='flex items-center'>
+          <img 
+            src={logo} 
+            alt="Logo" 
+            width={115} 
+            height={24} 
+            loading="lazy" 
+            className='pt-2 w-20 md:w-36' // Adjust width for small and medium screens
+          />
         </Link>
-        <div className='flex space-x-4 mr-8 h-full'>
+        <div className='hidden md:flex space-x-4 mr-8 h-full'>
           {links.map((link) => (
             <Link key={link.name} to={link.path} className='flex text-[19px] text-orange-100 font-medium nav-link items-center justify-center px-4'>
+              {link.name}
+            </Link>
+          ))}
+        </div>
+        
+
+        {/* Hamburger Button */}
+        <button
+          className='md:hidden flex items-center'
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? (
+            <HiX className='text-3xl text-black' />
+          ) : (
+            <GiHamburgerMenu className='text-3xl text-black' />
+          )}
+        </button>
+      </div>
+
+      {/* Sidebar */}
+      <div className={`fixed top-16 left-0 w-full bg-white shadow-lg transition-transform duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:hidden`}>
+        <div className='flex flex-col items-center p-8 space-y-10 justify-center'>
+          {links.map((link) => (
+            <Link key={link.name} to={link.path} className='text-[19px] text-orange-100 font-medium py-2' onClick={() => setIsOpen(false)}>
               {link.name}
             </Link>
           ))}
