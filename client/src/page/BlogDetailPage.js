d083af0ff934065a73b3d14dbec43ae89aa7310a
Harsh <harshku2222@gmail.com>
2024-07-21 08:08:01 +0530

    fix: solved the isssue of the popup.

--- client/src/page/BlogDetailPage.js
+++ client/src/page/BlogDetailPage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import EmailSender from '../components/core/Blog/EmailSender';
 import RecentBlogs from '../components/core/Blog/RecentBlogs';
 import BlogFullDescription from '../components/core/BlogDetail/BlogFullDescription';
@@ -6,21 +7,29 @@ import Popup from '../components/Popup';
 import ShareBar from '../components/core/BlogDetail/ShareBar';
 
 const BlogDetail = () => {
+    const { id } = useParams(); // Get the blog ID from the route parameters
     const [showPopup, setShowPopup] = useState(false);
+    const [popupCount, setPopupCount] = useState(0);
 
     useEffect(() => {
         const handleScroll = () => {
             const scrollPosition = window.scrollY + window.innerHeight;
             const pageHeight = document.documentElement.scrollHeight;
 
-            if (scrollPosition > pageHeight / 2 && !showPopup) {
+            if (scrollPosition > pageHeight / 2 && popupCount < 1) {
                 setShowPopup(true);
+                setPopupCount(popupCount + 1);
             }
         };
 
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
-    }, [showPopup]);
+    }, [popupCount]);
+
+    useEffect(() => {
+        // Reset the popup count when the route changes
+        setPopupCount(0);
+    }, [id]);
 
     const closePopup = () => {
         setShowPopup(false);
